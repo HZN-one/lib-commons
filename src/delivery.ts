e4fbe6bcc124b5a41d22e03e1b9fc98513b67f3a
didikmulyadi <didikmulyadi12@gmail.com>
2021-10-05 10:55:10 +0700

    Add delivery interface

--- src/delivery.ts
+++ src/delivery.ts
@@ -6,6 +6,52 @@ export interface IPriceStandarObject {
   amount: string;
   distance: number;
 }
+export interface ICoordinate {
+  latitude: number;
+  longitude: number;
+}
+export interface IAddress {
+  address?: string;
+  keywords?: string;
+  coordinate: ICoordinate;
+  city?: string;
+  district?: string;
+  postalCode?: string;
+}
+
+export interface IContact {
+  firstName: string;
+  lastName?: string;
+  email: string;
+  phone: string;
+  title?: string;
+  companyName?: string;
+  instruction?: string;
+}
+
+export interface IItem {
+  name?: string;
+  quantity: number;
+  description?: string;
+  price: number;
+  invoiceNo?: string;
+  dimension: {
+    height: number;
+    width: number;
+    depth: number;
+    weight: number;
+  };
+}
+export interface IDestination extends IAddress {
+  deliveryID?: string;
+  recipient: IContact;
+  items: IItem[];
+}
+export interface IPriceRequestBody {
+  sender: IContact;
+  origin: IAddress;
+  destinations: IDestination[];
+}
 
 export const Delivery = {
   /**
