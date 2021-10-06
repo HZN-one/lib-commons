936009a4811efa3f5ee99a18f85c470e08ddf00d
didikmulyadi <didikmulyadi12@gmail.com>
2021-10-06 10:56:32 +0700

    Add util.pick

---
+++ src/util.ts
@@ -0,0 +1,11 @@
+export const Util = {
+  pick: (object: any, keys: any): any => {
+    return keys.reduce((obj: any, key: number) => {
+      if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+        // eslint-disable-next-line no-param-reassign
+        obj[key] = object[key];
+      }
+      return obj;
+    }, {});
+  },
+};
