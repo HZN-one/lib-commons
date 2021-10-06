e0299ba5a1dbf24b4eb4e6455e34484cbb8941ca
didikmulyadi <didikmulyadi12@gmail.com>
2021-10-06 12:30:08 +0700

    Add stack in ApiError

--- src/error.ts
+++ src/error.ts
@@ -2,15 +2,21 @@ export class ApiError extends Error {
   message: string;
   statusCode: number;
   isOperational: boolean;
+  stack: string;
 
   constructor(
     statusCode: number,
     message: string,
-    isOperational: boolean = true
+    isOperational: boolean = true,
+    stack: string = ""
   ) {
     super(message);
     this.message = message;
     this.statusCode = statusCode;
     this.isOperational = isOperational;
+
+    if (stack) {
+      this.stack = stack;
+    }
   }
 }
